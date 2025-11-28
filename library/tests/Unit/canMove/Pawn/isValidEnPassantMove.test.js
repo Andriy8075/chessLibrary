bd56967ec7a8072109a7c157766bffe7575841e7
andriy8075 <andriy8075@gmail.com>
2025-11-28 23:34:44 +0200

    extra isValidEnPassantMove test

--- library/tests/Unit/canMove/Pawn/isValidEnPassantMove.test.js
+++ library/tests/Unit/canMove/Pawn/isValidEnPassantMove.test.js
@@ -37,18 +37,12 @@ test('can capture only if row is 3 or 6', () => {
     }
 })
 
-// test('can capture only if column is the same as the en passant target', () => {
-//     const board = new MockBoard([
-//         {type: 'pawn', color: 'white', position: { row: 5, col: 1 }},
-//         {type: 'pawn', color: 'black', position: { row: 5, col: 2 }},
-//     ], {
-//         enPassantTarget: { row: 4, col: 1 }
-//     });
-//     for (let i = 1; i <= 8; i++) {
-//         for (let j = 1; j <= 8; j++) {
-//             const cell = { row: i, col: j };
-//             result = Pawn.isValidEnPassantMove({ row: 5, col: 2 }, cell, board);
-//             expect(result).toBe(j === 1);
-//         }
-//     }
-// })
+test('can not en passant if enPassantTarget is not set', () => {
+    const board = new MockBoard([
+        {type: 'pawn', color: 'white', position: { row: 4, col: 1 }},
+        {type: 'pawn', color: 'black', position: { row: 4, col: 2 }},
+    ]);
+    const cell = { row: 3, col: 1 };
+    result = Pawn.isValidEnPassantMove({ row: 4, col: 2 }, cell, board);
+    expect(result).toBe(false);
+})
