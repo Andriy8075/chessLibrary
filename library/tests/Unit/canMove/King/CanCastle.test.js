27101c56b59b742db2ce12130558617b69c908c9
andriy8075 <andriy8075@gmail.com>
2025-11-28 19:04:01 +0200

    more tests for castling

--- library/tests/Unit/canMove/King/CanCastle.test.js
+++ library/tests/Unit/canMove/King/CanCastle.test.js
@@ -12,7 +12,81 @@ test('white king can castle kingside', () => {
         }
     });
     const king = board.getPieceOnCell({ row: 1, col: 4 });
+    result = king.canCastle({ row: 1, col: 2 });
+    expect(result).toBe(true);
+})
+
+test('white king can castle queenside', () => {
+    const board = new MockBoard([
+        {type: 'king', color: 'white', position: { row: 1, col: 4 }},
+        {type: 'rook', color: 'white', position: { row: 1, col: 8 }},
+    ], {
+        piecesMadeMoves: {
+            whiteKing: false,
+            whiteQueensideRook: false,
+        }
+    });
+    const king = board.getPieceOnCell({ row: 1, col: 4 });
     result = king.canCastle({ row: 1, col: 6 });
     expect(result).toBe(true);
 })
 
+test('black king can castle kingside', () => {
+    const board = new MockBoard([
+        {type: 'king', color: 'black', position: { row: 8, col: 4 }},
+        {type: 'rook', color: 'black', position: { row: 8, col: 1 }},
+    ], {
+        piecesMadeMoves: {
+            blackKing: false,
+            blackKingsideRook: false,
+        }
+    });
+    const king = board.getPieceOnCell({ row: 8, col: 4 });
+    result = king.canCastle({ row: 8, col: 2 });
+    expect(result).toBe(true);
+})
+
+test('black king can castle queenside', () => {
+    const board = new MockBoard([
+        {type: 'king', color: 'black', position: { row: 8, col: 4 }},
+        {type: 'rook', color: 'black', position: { row: 8, col: 8 }},
+    ], {
+        piecesMadeMoves: {
+            blackKing: false,
+            blackQueensideRook: false,
+        }
+    });
+    const king = board.getPieceOnCell({ row: 8, col: 4 });
+    result = king.canCastle({ row: 8, col: 6 });
+    expect(result).toBe(true);
+})
+
+test('can not castle if king has moved', () => {
+    const board = new MockBoard([
+        {type: 'king', color: 'white', position: { row: 1, col: 4 }},
+        {type: 'rook', color: 'white', position: { row: 1, col: 1 }},
+    ], {
+        piecesMadeMoves: {
+            whiteKing: true,
+            whiteKingsideRook: false,
+        }
+    });
+    const king = board.getPieceOnCell({ row: 1, col: 4 });
+    result = king.canCastle({ row: 1, col: 2 });
+    expect(result).toBe(false);
+})
+
+test('can not castle if rook has moved', () => {
+    const board = new MockBoard([
+        {type: 'king', color: 'white', position: { row: 1, col: 4 }},
+        {type: 'rook', color: 'white', position: { row: 1, col: 1 }},
+    ], {
+        piecesMadeMoves: {
+            whiteKing: false,
+            whiteKingsideRook: true,
+        }
+    });
+    const king = board.getPieceOnCell({ row: 1, col: 4 });
+    result = king.canCastle({ row: 1, col: 2 });
+    expect(result).toBe(false);
+})
