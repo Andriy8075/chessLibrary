a9d31dc0a3f95278bcbdb48095dc35c7bcdebdad
andriy8075 <andriy8075@gmail.com>
2025-11-29 14:08:11 +0200

    Queen test cases

---
+++ library/tests/Unit/boards/Queen/CanCaptureEnemyPiecesButNotOwn.js
@@ -0,0 +1,28 @@
+CanCaptureEnemyPiecesButNotOwn = {
+    pieces: [
+        {type: 'queen', color: 'white', position: {row: 3, col: 2}},
+        {type: 'knight', color: 'black', position: {row: 4, col: 2}},
+        {type: 'queen', color: 'white', position: {row: 4, col: 3}},
+        {type: 'knight', color: 'black', position: {row: 5, col: 2}},
+        {type: 'bishop', color: 'white', position: {row: 5, col: 3}},
+        {type: 'rook', color: 'black', position: {row: 5, col: 4}}
+    ],    mainPiecePosition: {row: 4, col: 3},
+    moves: [
+        {row: 3, col: 3},
+        {row: 2, col: 3},
+        {row: 1, col: 3},
+        {row: 4, col: 4},
+        {row: 4, col: 5},
+        {row: 4, col: 6},
+        {row: 4, col: 7},
+        {row: 4, col: 8},
+        {row: 3, col: 4},
+        {row: 2, col: 5},
+        {row: 1, col: 6},
+        {row: 4, col: 2},
+        {row: 5, col: 2},
+        {row: 5, col: 4}
+    ]
+}
+
+module.exports = CanCaptureEnemyPiecesButNotOwn;
