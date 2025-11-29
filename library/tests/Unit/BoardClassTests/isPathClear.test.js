b1629e37d9e0cc1ff968a4f59c77269928717d08
andriy8075 <andriy8075@gmail.com>
2025-11-29 21:24:09 +0200

    isPathClearTests

---
+++ library/tests/Unit/BoardClassTests/isPathClear.test.js
@@ -0,0 +1,54 @@
+const MockBoard = require('../../helpers/MockBoard');
+
+describe('Board.isPathClear', () => {
+    it('should return true if the path is clear. The same row', () => {
+        const board = new MockBoard([
+            {type: 'Rook', color: 'black', position: { row: 4, col: 3 }},
+            {type: 'pawn', color: 'white', position: { row: 4, col: 7 }},
+        ]);
+        expect(board.isPathClear({ row: 4, col: 3 }, { row: 4, col: 7 })).toBe(true);
+    });
+
+    it('should return false if the path is not clear. The same row', () => {
+        const board = new MockBoard([
+            {type: 'Rook', color: 'black', position: { row: 4, col: 3 }},
+            {type: 'pawn', color: 'black', position: { row: 4, col: 4 }},
+            {type: 'pawn', color: 'white', position: { row: 4, col: 7 }},
+        ]);
+        expect(board.isPathClear({ row: 4, col: 3 }, { row: 4, col: 7 })).toBe(false);
+    });
+
+    it('should return true if the path is clear. The same column', () => {
+        const board = new MockBoard([
+            {type: 'Rook', color: 'black', position: { row: 3, col: 3 }},
+            {type: 'pawn', color: 'white', position: { row: 7, col: 3 }},
+        ]);
+        expect(board.isPathClear({ row: 3, col: 3 }, { row: 7, col: 3 })).toBe(true);
+    });
+
+    it('should return false if the path is not clear. The same column', () => {
+        const board = new MockBoard([
+            {type: 'Rook', color: 'black', position: { row: 3, col: 3 }},
+            {type: 'pawn', color: 'black', position: { row: 4, col: 3 }},
+            {type: 'pawn', color: 'white', position: { row: 7, col: 3 }},
+        ]);
+        expect(board.isPathClear({ row: 3, col: 3 }, { row: 7, col: 3 })).toBe(false);
+    });
+
+    it('should return true if the path is clear. The same diagonal', () => {
+        const board = new MockBoard([
+            {type: 'Rook', color: 'black', position: { row: 2, col: 3 }},
+            {type: 'pawn', color: 'white', position: { row: 6, col: 7 }},
+        ]);
+        expect(board.isPathClear({ row: 2, col: 3 }, { row: 6, col: 7 })).toBe(true);
+    });
+
+    it('should return false if the path is not clear. The same diagonal', () => {
+        const board = new MockBoard([
+            {type: 'Rook', color: 'black', position: { row: 2, col: 3 }},
+            {type: 'pawn', color: 'black', position: { row: 3, col: 4 }},
+            {type: 'pawn', color: 'white', position: { row: 6, col: 7 }},
+        ]);
+        expect(board.isPathClear({ row: 2, col: 3 }, { row: 6, col: 7 })).toBe(false);
+    });
+});
