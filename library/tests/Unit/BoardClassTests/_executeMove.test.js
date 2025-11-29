be6c60c292ef6010d2915a4d2b7eafa4848cae74
andriy8075 <andriy8075@gmail.com>
2025-11-29 21:57:04 +0200

    _executeMove tests

---
+++ library/tests/Unit/BoardClassTests/_executeMove.test.js
@@ -0,0 +1,24 @@
+const MockBoard = require('../../helpers/MockBoard');
+
+describe('Board._executeMove', () => {
+    it('should move a piece from one cell to another', () => {
+        const board = new MockBoard([
+            {type: 'Pawn', color: 'white', position: { row: 2, col: 4 }},
+        ]);
+        const pawn = board.getPieceOnCell({ row: 2, col: 4 });
+        board._executeMove({ row: 2, col: 4 }, { row: 3, col: 4 });
+        expect(board.getPieceOnCell({ row: 3, col: 4 })).toBe(pawn);
+        expect(board.getPieceOnCell({ row: 2, col: 4 })).toBeNull();
+    });
+
+    it('should capture a piece if it is on the target cell', () => {
+        const board = new MockBoard([
+            {type: 'Pawn', color: 'white', position: { row: 2, col: 4 }},
+            {type: 'Pawn', color: 'black', position: { row: 3, col: 5 }},
+        ]);
+        const pawn = board.getPieceOnCell({ row: 2, col: 4 });
+        board._executeMove({ row: 2, col: 4 }, { row: 3, col: 4 });
+        expect(board.getPieceOnCell({ row: 3, col: 4 })).toBe(pawn);
+        expect(board.getPieceOnCell({ row: 2, col: 5 })).toBeNull();
+    });
+});
