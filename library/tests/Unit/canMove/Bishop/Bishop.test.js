d3f116517771e2744ab0e97c7fbaff2f6b4a2628
andriy8075 <andriy8075@gmail.com>
2025-11-29 14:15:08 +0200

    delete duplicate board tests

--- library/tests/Unit/canMove/Bishop/Bishop.test.js
+++ library/tests/Unit/canMove/Bishop/Bishop.test.js
@@ -60,28 +60,3 @@ test('can not jump over pieces', () => {
     result = bishop.canMove({ row: 6, col: 5 });
     expect(result).toBe(false);
 });
-
-test('mock board cases', () => {
-    const boards = loadMockBoards('Unit/canMove/Bishop/boards');
-    for (const board of boards) {
-        const bishop = board.board.getPieceOnCell(board.mainPiecePosition);
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                const cellTo = { row: i + 1, col: j + 1 };
-                result = bishop.canMove(cellTo);
-                let expected = false;
-                for (const move of board.moves) {
-                    if (cellsEqual(move, cellTo)) {
-                        expected = true;
-                        break;
-                    }
-                }
-                if (result !== expected) {
-                    console.log(`Bishop at ${board.mainPiecePosition} can move to ${cellTo} but should not`, {depth: 5});
-                }
-                expect(result).toBe(expected);
-
-            }
-        }
-    }
-});
