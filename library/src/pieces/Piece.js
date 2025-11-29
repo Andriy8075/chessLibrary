1ae07a7b2ad175d7d2ec02f401ad2a7cc7f64d8b
andriy8075 <andriy8075@gmail.com>
2025-11-29 12:22:12 +0200

    correct import for case sensitive linux

--- library/src/pieces/Piece.js
+++ library/src/pieces/Piece.js
@@ -1,5 +1,5 @@
 const { isValidCell } = require('../utils/Cell');
-const { InvalidColorError, InvalidCellError, NotImplementedMethodError } = require('../Errors');
+const { InvalidColorError, InvalidCellError, NotImplementedMethodError } = require('../errors');
 
 
 class Piece {
