dcc4e72e4471db65432523b5078065f7adee9ffb
andriy8075 <andriy8075@gmail.com>
2025-12-01 20:31:40 +0200

    delete unused imports

--- library/tests/Unit/CheckDetector/isKingInCheck.test.js
+++ library/tests/Unit/CheckDetector/isKingInCheck.test.js
@@ -1,8 +1,5 @@
 const loadMockBoards = require('../../helpers/loadMockBoards');
 const CheckDetector = require('../../../src/board/CheckDetector');
-const path = require('path');
-const fs = require('fs');
-const mockBoardsFolder = require('../../helpers/mockBoardsFolder');
 
 test('board cases', () => {
     const boards = loadMockBoards('isKingInCheck');
