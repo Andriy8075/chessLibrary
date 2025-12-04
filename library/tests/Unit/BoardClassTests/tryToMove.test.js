e09f11dd2e388b44c85755f856f88a7bab6b23da
andriy8075 <andriy8075@gmail.com>
2025-12-04 16:46:55 +0200

    check updating en passant after trying to move

--- library/tests/Unit/BoardClassTests/tryToMove.test.js
+++ library/tests/Unit/BoardClassTests/tryToMove.test.js
@@ -6,13 +6,28 @@ test('board cases', () => {
         let result
         if (board.promotionPiece) {
             result = board.board.tryToMove(board.cellFrom, board.cellTo, board.promotionPiece);
-            if (result) {
-                const newPiece = board.board.getPieceOnCell(board.cellTo);
-                expect(newPiece.constructor.name.toLowerCase()).toBe(board.promotionPiece.toLowerCase());
-            }
         } else {
             result = board.board.tryToMove(board.cellFrom, board.cellTo);
         }
+
         expect(result).toBe(board.expectedResult);
+
+        if (result) {
+            const movedPiece = board.board.getPieceOnCell(board.cellTo);
+            if(movedPiece.constructor.name.toLowerCase() === 'pawn'){
+                if (Math.abs(board.cellTo.row - board.cellFrom.row) === 2) {
+                    const realEnPassantTarget = board.board.getEnPassantTarget();
+                    const expectedEnPassantTarget = {
+                        row: board.cellTo.row + (board.cellFrom.color === 'white' ? 1 : -1),
+                         col: board.cellTo.col
+                    };
+                    expect(realEnPassantTarget).toEqual(expectedEnPassantTarget);
+                }
+
+                if(board.cellTo.row === (board.cellFrom.color === 'white' ? 8 : 1)){
+                    expect(movedPiece.constructor.name.toLowerCase()).toBe(board.promotionPiece.toLowerCase());
+                }
+            }
+        }
     }
 });
