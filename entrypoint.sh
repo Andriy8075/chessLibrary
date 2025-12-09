cea29e286676f11f1d78f2664c425a25e00f85a7
andriy8075 <andriy8075@gmail.com>
2025-12-09 13:47:13 +0200

    launch singlePlayerWeb in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ npm install
 cd ../library/tests/helpers/mockBoardEditor
 npm install
 npm run build
+cd /app/library/tests/singlePlayerWeb
+npm install
+node server.js &
 cd /app
 npm install
 npm run dev
